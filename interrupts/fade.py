b73e2e7f02f6be1b8c04fb51714ede5aad13a636
Daniel Chege <dannduati2@gmail.com>
2020-07-08 12:45:04 +0300

    Add files via upload

---
+++ interrupts/fade.py
@@ -0,0 +1,19 @@
+from machine import Pin, PWM, ADC
+from time import sleep
+
+frequency = 5000
+led = PWM(Pin(5), frequency)
+pot = ADC(Pin(34))
+pot.width(ADC.WIDTH_10BIT)
+pot.atten(ADC.ATTN_11DB)
+
+while True:
+  pot_value = pot.read()
+  print(pot_value)
+
+  if pot_value < 15:
+    led.duty(0)
+  else:
+    led.duty(pot_value)
+
+  sleep(0.1)
